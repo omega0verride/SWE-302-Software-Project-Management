b3dc187cdebd600ba4d4a4c57b484b7c59846bd9
rei kurti <rkurti20@epoka.edu.al>
2023-04-10 14:52:50 +0200

    FormTitle Component

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -4,6 +4,7 @@ import React, { useEffect } from 'react'
 import LoginButton from '../components/loginButton'
 import UsernameTextField from '../components/UsernameTextField'
 import TextFieldLabel from '../components/TextFieldLabel'
+import FormTitle from '../components/FormTitle'
 
 export default function Home() {
 
@@ -16,6 +17,7 @@ export default function Home() {
   return (
       <div>
           <LoginButton></LoginButton>
+          <FormTitle title='Create Account'></FormTitle>
           <TextFieldLabel text="Username"></TextFieldLabel>
           <UsernameTextField placeholder='Username'></UsernameTextField>
           
