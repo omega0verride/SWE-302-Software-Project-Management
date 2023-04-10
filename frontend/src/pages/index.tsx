ab41c4c3686eac9daeddf819d94effa86848d10a
rei kurti <rkurti20@epoka.edu.al>
2023-04-10 15:20:20 +0200

    ShopName Component

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -6,6 +6,7 @@ import UsernameTextField from '../components/UsernameTextField'
 import TextFieldLabel from '../components/TextFieldLabel'
 import FormTitle from '../components/FormTitle'
 import PageLogo from '../components/PageLogo'
+import ShopName from '../components/ShopName'
 
 export default function Home() {
 
@@ -18,6 +19,7 @@ export default function Home() {
   return (
       <div>
           <PageLogo></PageLogo>
+          <ShopName></ShopName>
           <LoginButton></LoginButton>
           <FormTitle title='Create Account'></FormTitle>
           <TextFieldLabel text="Username"></TextFieldLabel>
