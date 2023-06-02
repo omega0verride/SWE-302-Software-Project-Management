4a426560dc71e846d09b6317a7d012167f328e85
omega <om3ga0verride@gmail.com>
2023-06-02 20:23:45 +0200

    fixed confirmation url

--- backend/src/main/java/com/redscooter/API/appUser/registration/RegistrationListener.java
+++ backend/src/main/java/com/redscooter/API/appUser/registration/RegistrationListener.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponents;
+
+import java.net.URL;
 
 @Component
 public class RegistrationListener implements ApplicationListener<OnRegistrationCompleteEvent> {
@@ -26,7 +29,9 @@ public class RegistrationListener implements ApplicationListener<OnRegistrationC
         VerificationToken verificationToken = event.getVerificationToken();
         AppUser user = verificationToken.getUser();
 
-        String confirmationURL = ServletUriComponentsBuilder.fromRequestUri(event.getHttpServletRequest()).build().toUriString() + "/api/users/register/confirm?token=" + verificationToken.getToken() + "&username=" + user.getUsername();
+        UriComponents uriComponents = ServletUriComponentsBuilder.fromRequestUri(event.getHttpServletRequest()).build();
+        String port = uriComponents.getPort() == -1 ? "" : ":" + uriComponents.getPort();
+        String confirmationURL = uriComponents.getScheme() + "://" + uriComponents.getHost() + port + "/api/users/register/confirm?token=" + verificationToken.getToken() + "&username=" + user.getUsername();
         String recipientAddress = user.getUsername();
         String subject = "Konfirmimi i regjistrimit ne RedScooter.";
         emailSender.sendEmailWithDefaultExceptionHandler(recipientAddress, subject, ConfirmUserRegistrationHTMLBuilder.buildConfirmUserRegistrationHTML(user, confirmationURL, verificationToken));
--- backend/src/test/java/com/redscooter/api/TestFileStore.java
+++ backend/src/test/java/com/redscooter/api/TestFileStore.java
@@ -4,6 +4,7 @@ import com.redscooter.util.Utilities;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -12,7 +13,10 @@ public class TestFileStore {
     public void testPathNormalize() {
         Path rootAllowdPath = Paths.get("/root/filestore");
         Assertions.assertFalse(Utilities.validatePathPermissions(Paths.get(rootAllowdPath.toString(), "../schema.ini").normalize(), rootAllowdPath));
-        Assertions.assertTrue(Utilities.validatePathPermissions(Paths.get(rootAllowdPath.toString(), "..\\schema.ini").normalize(), rootAllowdPath));
+        if (File.separator.equals("\\"))
+            Assertions.assertFalse(Utilities.validatePathPermissions(Paths.get(rootAllowdPath.toString(), "..\\schema.ini").normalize(), rootAllowdPath));
+        else
+            Assertions.assertTrue(Utilities.validatePathPermissions(Paths.get(rootAllowdPath.toString(), "..\\schema.ini").normalize(), rootAllowdPath));
         Assertions.assertFalse(Utilities.validatePathPermissions(Paths.get(rootAllowdPath.toString(), "../../schema.ini").normalize(), rootAllowdPath));
         Assertions.assertTrue(Utilities.validatePathPermissions(Paths.get(rootAllowdPath.toString(), "./schema.ini").normalize(), rootAllowdPath));
         Assertions.assertTrue(Utilities.validatePathPermissions(Paths.get(rootAllowdPath.toString(), "/schema.ini").normalize(), rootAllowdPath));
