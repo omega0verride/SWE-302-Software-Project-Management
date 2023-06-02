36d9528e6db9cec5de523363592469d2334b3c50
ibreti <indritbreti@gmail.com>
2023-06-02 14:19:37 +0200

    backup

--- backend/src/test/java/com/redscooter/api/TestFileStore.java
+++ backend/src/test/java/com/redscooter/api/TestFileStore.java
@@ -1,5 +1,21 @@
 package com.redscooter.api;
 
+import com.redscooter.util.Utilities;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class TestFileStore {
+    @Test
+    public void testPathNormalize() {
+        Path rootAllowdPath = Paths.get("/root/filestore");
+        Assertions.assertFalse(Utilities.validatePathPermissions(Paths.get(rootAllowdPath.toString(), "../schema.ini").normalize(), rootAllowdPath));
+        Assertions.assertTrue(Utilities.validatePathPermissions(Paths.get(rootAllowdPath.toString(), "..\\schema.ini").normalize(), rootAllowdPath));
+        Assertions.assertFalse(Utilities.validatePathPermissions(Paths.get(rootAllowdPath.toString(), "../../schema.ini").normalize(), rootAllowdPath));
+        Assertions.assertTrue(Utilities.validatePathPermissions(Paths.get(rootAllowdPath.toString(), "./schema.ini").normalize(), rootAllowdPath));
+        Assertions.assertTrue(Utilities.validatePathPermissions(Paths.get(rootAllowdPath.toString(), "/schema.ini").normalize(), rootAllowdPath));
+    }
 
 }
