383d8a4184df536a6b565b5b94fabfe3f9837c27
rei kurti <rkurti20@epoka.edu.al>
2023-06-03 22:16:33 +0200

    apollo & gql install

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,11 +1,18 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
 import { wrapper } from '../store/store'
 import React, { Fragment } from 'react'
 
+
+const client = new ApolloClient({
+  uri: 'YOUR_API_ENDPOINT', // Replace with your API endpoint
+  cache: new InMemoryCache(),
+});
+
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Fragment>
+  <ApolloProvider client={client}>
     <Component {...pageProps} />
-  </Fragment>
+  </ApolloProvider>
 )
 
 export default wrapper.withRedux(MyApp)
