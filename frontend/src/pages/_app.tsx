589580c348869cb9eda5e3e885fd026861d92584
rei kurti <rkurti20@epoka.edu.al>
2023-06-04 10:22:23 +0200

    reverting the use of apollo client and gql

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,18 +1,12 @@
 import type { AppProps } from 'next/app';
-import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
 import { wrapper } from '../store/store'
 import React, { Fragment } from 'react'
 
 
-const client = new ApolloClient({
-  uri: 'YOUR_API_ENDPOINT', // Replace with your API endpoint
-  cache: new InMemoryCache(),
-});
-
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <ApolloProvider client={client}>
+  <Fragment>
     <Component {...pageProps} />
-  </ApolloProvider>
+  </Fragment>
 )
 
 export default wrapper.withRedux(MyApp)
