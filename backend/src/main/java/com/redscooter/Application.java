efb8bd61a539cd7c68e94b963bf31eebce32027f
omega0verride <om3ga0verride@gmail.com>
2023-10-15 00:12:17 +0200

    test

--- backend/src/main/java/com/redscooter/Application.java
+++ backend/src/main/java/com/redscooter/Application.java
@@ -1,5 +1,6 @@
 package com.redscooter;
 
+
 import org.restprocessors.EnableRestProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
